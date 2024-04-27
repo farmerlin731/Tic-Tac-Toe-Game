e53968927446f464a2e873d78ac5645a1649cb2d
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-04-27 18:37:30 +0800

    Clean code.

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,29 @@ function deriveActivepPlayer(turnState) {
   }
   return currentPlayer;
 }
+function deriveGameData(turnState) {
+  const gameData = Array.from({ length: 3 }, () => Array(3).fill(null));
+  //the right way to watch the content of an 'object' if the object will be adjust in this render.
+  // gameData.forEach((arr) => arr.forEach((item) => console.log(item)));
+  // console.log(gameData);
+  for (const data of turnState) {
+    const { square, player } = data;
+    const { rowIndex, colIndex } = square;
+    gameData[rowIndex][colIndex] = player;
+  }
+  return gameData;
+}
+
+function deriveWinner(gameData) {
+  let winner;
+  WINNING_COMBINATIONS.forEach((winning) => {
+    let symbol0 = gameData[winning[0].row][winning[0].column];
+    let symbol1 = gameData[winning[1].row][winning[1].column];
+    let symbol2 = gameData[winning[2].row][winning[2].column];
+    if (symbol0 && symbol0 == symbol1 && symbol1 == symbol2) winner = symbol0;
+  });
+  return winner;
+}
 
 function App() {
   // const [activePlayer, setActivePlayer] = useState("X");
@@ -24,26 +47,9 @@ function App() {
   const [playerName, setPlayerName] = useState(INIT_PLAYER_NAME);
 
   const activePlayer = deriveActivepPlayer(turnState);
-  const gameData = Array.from({ length: 3 }, () => Array(3).fill(null));
-
-  //the right way to watch the content of an 'object' if the object will be adjust in this render.
-  // gameData.forEach((arr) => arr.forEach((item) => console.log(item)));
-  // console.log(gameData);
-
-  let winner;
+  const gameData = deriveGameData(turnState);
+  const winner = deriveWinner(gameData);
   const isDraw = turnState.length === 9 && !winner;
-  for (const data of turnState) {
-    const { square, player } = data;
-    const { rowIndex, colIndex } = square;
-    gameData[rowIndex][colIndex] = player;
-  }
-
-  WINNING_COMBINATIONS.forEach((winning) => {
-    let symbol0 = gameData[winning[0].row][winning[0].column];
-    let symbol1 = gameData[winning[1].row][winning[1].column];
-    let symbol2 = gameData[winning[2].row][winning[2].column];
-    if (symbol0 && symbol0 == symbol1 && symbol1 == symbol2) winner = symbol0;
-  });
 
   function handleSquareSelect(rowIndex, colIndex) {
     console.log(`push: ${rowIndex},${colIndex}`);
